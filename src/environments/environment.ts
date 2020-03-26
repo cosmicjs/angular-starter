54cb9cf4a5846f87882ea14842301355ab3f556e
Tony Spiro <tspiro@tonyspiro.com>
2020-03-26 10:05:31 -0500

    ADDED: import script

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  read_key: '##COSMIC_READ_KEY##',
+  read_key: 'Lra3068QQTkSSqXie3EE1uL3BWq22XKY06EhzhG54r7enn0gJM',
   write_key: '##COSMIC_WRITE_KEY##',
-  bucket_slug: '##COSMIC_BUCKET##',
+  bucket_slug: 'bddf7f70-6f71-11ea-9399-e3d37d49b96d',
   URL: 'https://api.cosmicjs.com/v1/'
 };
