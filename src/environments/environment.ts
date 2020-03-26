fc149ba9055a551b8e03154fe4f9c4d25c586aaf
Tony Spiro <tspiro@tonyspiro.com>
2020-03-26 10:28:18 -0500

    FIXED: semicolon hanger

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  read_key: 'BHZWdg7ZcdKpY2di0uLtnRh9PqYvALaEQK1IccJYf7RNyha1BE',
+  read_key: '##COSMIC_READ_KEY##',
   write_key: '##COSMIC_WRITE_KEY##',
-  bucket_slug: '88f49040-6f74-11ea-a79e-99343798db09',
+  bucket_slug: '##COSMIC_BUCKET##',
   URL: 'https://api.cosmicjs.com/v1/'
 };
